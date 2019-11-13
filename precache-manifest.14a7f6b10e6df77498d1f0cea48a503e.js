523170e1c752a95bc5202142f647cef350b4f09f
David <david@localhost.localdomain>
2019-11-13 15:29:46 -0600

    Updates

---
+++ precache-manifest.14a7f6b10e6df77498d1f0cea48a503e.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eddc7c2e86f21a216b8fbd2a510a2a4d",
+    "url": "/second-app/index.html"
+  },
+  {
+    "revision": "06a83d552ddb724b8116",
+    "url": "/second-app/static/css/2.4d9b5897.chunk.css"
+  },
+  {
+    "revision": "6004514c13971778f7b2",
+    "url": "/second-app/static/css/main.76717219.chunk.css"
+  },
+  {
+    "revision": "06a83d552ddb724b8116",
+    "url": "/second-app/static/js/2.d7e09d44.chunk.js"
+  },
+  {
+    "revision": "6004514c13971778f7b2",
+    "url": "/second-app/static/js/main.2876857a.chunk.js"
+  },
+  {
+    "revision": "e4df283d40ee6360f54f",
+    "url": "/second-app/static/js/runtime-main.c4a186b1.js"
+  },
+  {
+    "revision": "703d3a7e7eb39ead1c58cc5da9eed25d",
+    "url": "/second-app/static/media/img1.703d3a7e.jpg"
+  },
+  {
+    "revision": "c1f795080562d3cb6b855662447652f0",
+    "url": "/second-app/static/media/img2.c1f79508.jpg"
+  },
+  {
+    "revision": "abf374c2b6f62637cf90165869cd7cef",
+    "url": "/second-app/static/media/img3.abf374c2.jpg"
+  }
+]);
